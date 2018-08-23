b81e36c078b61531065d560dc48419b2626e187f
Spomi <40448323+Spomi@users.noreply.github.com>
2018-08-23 15:19:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,9 +15,22 @@ const youtube = new YouTube(GOOGLE_API_KEY);
 
 const queue = new Map();
 
-client.on('ready', function() {
-	console.log(`i am ready ${client.user.username}`);
-    client.user.setGame(prefix + 'Play for start|| 2help for info');
+client.on('ready', function(){
+    var ms = 5000 ;
+    var setGame = [`${prefix}help Servers ${client.guilds.size} `,`${prefix}invite Users ${client.users.size}`];
+    var i = -1;
+    var j = 0;
+    setInterval(function (){
+        if( i == -1 ){
+            j = 1;
+        }
+        if( i == (setGame.length)-1 ){
+            j = -1;
+        }
+        i = i+j;
+        client.user.setGame(setGame[i],`http://www.twitch.tv/osama_gmt`);
+    }, ms);5000
+
 });
 
 client.on('message', message => {
